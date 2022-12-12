e2d521bb059ecfac8442bb376bb0f89e2afbce2f
Yeldos <eldos.amire@gmail.com>
2022-12-12 06:32:27 +0600

    firebase

--- app/src/main/java/com/example/youdo/CreateCardFragment.kt
+++ app/src/main/java/com/example/youdo/CreateCardFragment.kt
@@ -1,14 +1,14 @@
 package com.example.youdo
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 
 class CreateCardFragment : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_create_card, container, false)
     }
--- app/src/main/java/com/example/youdo/YourTaskFragment.kt
+++ app/src/main/java/com/example/youdo/YourTaskFragment.kt
@@ -1,19 +1,15 @@
 package com.example.youdo
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 
 class YourTaskFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_your_task, container, false)
     }
-
 }
